56e103f735e753d7b7c984ace3b0349ebee4f1d6
abhijithavalon <abhijith.surendran@avalontribe.com>
2023-03-18 10:51:17 +0530

    first commit

---
+++ server/Controller/chatController.js
@@ -0,0 +1,48 @@
+const ChatModel = require("../Models/chatSchema");
+
+const createChat = async (req, res) => {
+  console.log(req.user,'reeeeeeeeeeeeeeeeeeeac');
+  console.log(req.body,'new chat bodyyyyy');
+  const newChat = new ChatModel({
+    members: [req.user._id, req.body.id],
+  });
+  try {
+    const chat = await ChatModel.findOne({
+      members: { $all: [req.user._id, req.body.id] },
+    });
+    if (!chat) {
+      const result = await newChat.save();
+      res.status(200).json(result);
+    } else {
+      res.status(200).json(chat);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const userChats = async (req, res) => {
+  try {
+    const chat = await ChatModel.find({
+      members: { $in: [req.params.userId] },
+    }).sort({ updatedAt: -1 })
+    // .populate('recent','senderId text image');
+    console.log(chat,'findeddd chattttt');
+    res.status(200).json(chat);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const findChat = async (req, res) => {
+  try {
+    const chat = await ChatModel.findOne({
+      members: { $all: [req.params.firstId, req.params.secondId] },
+    });
+    res.status(200).json(chat);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { findChat, userChats, createChat };
